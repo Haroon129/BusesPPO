b5075ea85f0496df8b406eb218eaffcf36716dc8
Tobi gdesoria <dani.gdesoriab@gmail.com>
2025-09-19 10:24:31 +0200

    añadido bus y billetes

---
+++ bus.py
@@ -0,0 +1,37 @@
+from Estacion import Estacion
+from billetes import Billetes
+
+class Buses:
+    def __init__(self, id, numAsientos):
+        self.__id = id
+        self.__numAsientos = numAsientos
+        self.__billete = []
+    
+    def calcularAsientosRestantes(self):
+        asientosRestantes = self.__numAsientos - len (self.__billete)
+        return asientosRestantes
+    
+    def billete_noDup(self, billete):
+        name = billete.get_name()
+        apellido = billete.get_apellido()
+        for i in range(len (self.__billete)):
+            if self.__billete[i].get_name() == name and self.__billete[i].get_apellido() == apellido:
+                return True
+        return False
+    
+    def destuir_billete(self, name, apellido):
+        for i in range(len (self.__billete)):
+            if self.__billete[i].get_name() == name and self.__billete[i].get_apellido() == apellido:
+                self.__billete.pop(i)
+                return True
+        return False
+
+    def anadir_billete(self, billete):
+        self.__billete.append(billete)
+    
+    def __str__(self):
+        return 
+        f"Id: {self.__id}\n"\
+        f"Total: {self.__numAsientos}\n"\
+        f"Libre: {self.calcularAsientosRestantes()}\n"\
+        f"Vendido: {len (self.__billete)}\n"
